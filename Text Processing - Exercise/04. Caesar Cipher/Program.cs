a5ed2b6f984e3c173e80afde9a36e157e084d683
Teodor <tedkata96@gmail.com>
2022-11-12 15:09:01 +0200

    Upload solution for 04. Caesar Cipher

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Caesar_Cipher
+{
+    using System;
+    using System.Text;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (var ch in text)
+            {
+                int currCh = (int)ch + 3;
+                char newChar = (char)currCh;
+                encryptedText.Append(newChar);
+            }
+
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
