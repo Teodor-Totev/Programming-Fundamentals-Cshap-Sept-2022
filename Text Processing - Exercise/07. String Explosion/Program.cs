a4e4a10dfc6d798a6fd71ec42d3314c3a93a6fa2
Teodor <tedkata96@gmail.com>
2022-11-14 16:14:17 +0200

    Upload solution for task
    07. String Explosion

---
+++ Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace P07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = Console.ReadLine();
+
+            StringBuilder outputText = new StringBuilder();
+            int bombPower = 0;
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                char currCh = inputStr[i];
+
+                if (currCh == '>')
+                {
+                    int currBombPower = GetIntValueOfCharacter(inputStr[i + 1]);
+
+                    outputText.Append(currCh);
+                    bombPower += currBombPower;
+                }
+                else
+                {
+                    if (bombPower > 0)
+                    {
+                        bombPower--;
+                    }
+                    else
+                    {
+                        outputText.Append(currCh);
+                    }
+                }
+            }
+
+            Console.WriteLine(outputText.ToString());
+        }
+
+        static int GetIntValueOfCharacter(char ch)
+        {
+            return (int)ch - 48;
+        }
+    }
+}
