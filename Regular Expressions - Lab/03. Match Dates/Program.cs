66cbb5755d67ab4086057385105831179c3d7ab4
Teodor <tedkata96@gmail.com>
2022-11-18 13:48:32 +0200

    Upload solution for task
    03. Match Dates

---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>[0-9]{2})((\.|-|/))(?<month>[A-Z]+[a-z]{2})\1(?<year>[0-9]{4})";
+
+            string dates = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection validDates = regex.Matches(dates);
+
+            foreach (Match item in validDates)
+            {
+                string day = item.Groups["day"].Value;
+                string month = item.Groups["month"].Value;
+                string year = item.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+
+            }
+        }
+    }
+}
