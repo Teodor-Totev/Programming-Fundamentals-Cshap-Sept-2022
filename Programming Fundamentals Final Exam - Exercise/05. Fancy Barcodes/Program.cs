4510c903ce8474703c1c07e015415b0361ab0b07
Teodor <tedkata96@gmail.com>
2022-11-29 22:27:53 +0200

    Upload solution for task
    Fancy Barcodes

---
+++ Programming Fundamentals Final Exam - Exercise/05. Fancy Barcodes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _05._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@#+(?<barcode>[A-Z]+[A-Za-z0-9]{4,}[A-Z]+)@#+";
+            //Regex pattern = new Regex(@"@#+(?<barcode>[A-Z]+[A-Za-z0-9]{4,}[A-Z]+)@#+);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string barcode = match.Groups["barcode"].Value;
+                    //char[] digits = barcode.Where(x => char.IsDigit(x)).ToArray();
+
+                    GetDigitOrLetterValueAndPrint(barcode);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+        static void GetDigitOrLetterValueAndPrint (string barcode)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < barcode.Length; i++)
+            {
+                if (char.IsDigit(barcode[i]))
+                {
+                    int currDigit = barcode[i] - '0';
+                    string currDigitToStr = currDigit.ToString();
+                    sb.Append(currDigitToStr);
+                }
+            }
+            if (sb.Length == 0)
+            {
+                Console.WriteLine("Product group: 00");
+            }
+            else
+            {
+                Console.WriteLine($"Product group: {sb}");
+            }
+        }
+    }
+}
