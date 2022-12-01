ccdcdced3700f54034e0e41e2396938b0252b34c
Teodor <tedkata96@gmail.com>
2022-12-01 16:03:03 +0200

    Upload solution for task
    Destination Mapper

---
+++ Programming Fundamentals Final Exam - Exercise/10. Destination Mapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _10._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)(?<destinations>[A-Z]{1}[A-Za-z]{2,})\1";
+
+            string input = Console.ReadLine();
+
+            MatchCollection destinations = Regex.Matches(input, pattern);
+
+            List<string> output = new List<string>();
+            int travelPoints = 0;
+
+            foreach (Match match in destinations)
+            {
+                string destination = match.Groups["destinations"].Value;
+                travelPoints += destination.Length;
+                output.Add(destination);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", output)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
